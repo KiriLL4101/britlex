36fba4916d29c943b7c7cfd703a43c4bf032d4b4
KiriLL4101 <fedechkin-kiril@mail.ru>
2023-02-22 21:12:01 +0300

    feat: layout main block

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import { Button } from './component/Button/Button'
 import { Menu } from './component/Menu/Menu'
 
-import { ReactComponent as Logo } from './assets/main-logo.svg'
+import { ReactComponent as LogoSvg } from './assets/main-logo.svg'
+import { ReactComponent as MainSvg } from './assets/main.svg'
 
 import styles from './app.module.css'
 
 function App() {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <Logo />
-        <Menu />
-        <Button text="Let’s Talk" />
-      </header>
-    </div>
+    <>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <LogoSvg />
+          <Menu />
+          <Button text="Let’s Talk" variant="secondary" />
+        </header>
+      </div>
+      <div className={styles.container}>
+        <section className={styles.block}>
+          <div className={styles.title}>
+            <h1>
+              Learn
+              <br /> Any Foreign Language
+            </h1>
+            <p>
+              With our teachers who write a program for each student, you will be able to make your
+              first sketch after the first lesson.
+            </p>
+            <Button text="Get started" size='l' />
+          </div>
+          <MainSvg />
+        </section>
+      </div>
+    </>
   )
 }
 
--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,11 +1,14 @@
 import { type FC } from 'react'
+import cn from 'classnames'
 
 import styles from './Button.module.css'
 
 interface ButtonProps {
   text: string
+  size?: 'm' | 'l'
+  variant?: 'primary' | 'secondary'
 }
 
-export const Button: FC<ButtonProps> = ({ text }) => {
-  return <button className={styles.button}>{text}</button>
+export const Button: FC<ButtonProps> = ({ text, size = 'm', variant = 'primary' }) => {
+  return <button className={cn(styles.button, styles[size], styles[variant])}>{text}</button>
 }
